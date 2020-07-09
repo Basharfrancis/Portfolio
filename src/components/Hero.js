fe2d68c74e105b0d2f9041174f4b3394ce1d8f8c
Bashar Francis <bfrancis@nuundigital.com>
2020-07-09 17:11:25 -0600

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
           <h1 className="hero-title">
             Hi, my name is <span className="text-color-main">Bashar Francis</span>
             <br />
-            I'm the Full-Stack Developer.
+            I'm the Software Developer.
           </h1>
           <p className="hero-cta">
             <a className="cta-btn cta-btn--hero" href="#about">
